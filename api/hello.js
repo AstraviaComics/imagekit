e6d352a5862709083ae56352832e6277e1940a0a
AstraviaComics <astraviacomic@gmail.com>
2025-05-14 15:45:47 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,19 @@
-module.exports = (req, res) => {
-  res.status(200).send("Hello from Serverless Function");
+// /api/hello.js
+import ImageKit from "imagekit";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    // Mengambil authentication parameters dari ImageKit
+    const result = imagekit.getAuthenticationParameters();
+    res.status(200).json(result); // Mengirimkan hasil signature
+  } else {
+    // Response default
+    res.status(200).send("Hello from Serverless Function");
+  }
 };
